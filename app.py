58edd80a44f19061f144e7dfb56625bd610560de
Matsumoto Eisuke (松本 英介) <matsumoto.eisuke@jp.panasonic.com>
2025-05-27 17:49:34 +0900

    demo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Docker and GitHub Actions!"
+    return "Hello, Docker and GitHub Actions!（Jenkins成功）"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
